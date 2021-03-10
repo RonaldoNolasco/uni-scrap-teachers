62de48010365c3d7fb4a4fd268388246fcb03ffa
TheBestShooter <rnolascoc@uni.pe>
2021-03-10 03:37:00 -0500

    definiendo funciones

--- scrap.py
+++ scrap.py
@@ -5,22 +5,36 @@ import json
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#Hallando los códigos de todos los profes
-facultades = ['A','C','E','G','I','L','M','N','P','Q','S']
-hojasPorFacultad = [19,30,20,16,19,21,23,21,9,13,19]
 codigos = []
 
-cookies = dict(PHPSESSID='4pcpkms76g5r9f8jki1a5s7292')
+def hallarCodigos():
+    #Hallando los códigos de todos los profes
+    facultades = ['A','C','E','G','I','L','M','N','P','Q','S']
+    hojasPorFacultad = [19,30,20,16,19,21,23,21,9,13,19]
 
-for i in range(0,len(facultades)):
-    for j in range(0,hojasPorFacultad[i]):
-        url = "https://www.orce.uni.edu.pe/profFacultad.php?flag=profFacu&facul=" + facultades[i] + "&pag=" + str(j+1)
-        response = r.get(url, cookies=cookies, verify=False)
-        response.encoding = 'utf-8'
-        soup = BeautifulSoup(response.text, 'html.parser')
-        print(soup)
+    cookies = dict(PHPSESSID='4pcpkms76g5r9f8jki1a5s7292')
 
-# 4 kind of objects Tag, NavigableString, BeautifulSoup, and Comment
+    for i in range(0,len(facultades)):
+        for j in range(0,hojasPorFacultad[i]):
+            url = "https://www.orce.uni.edu.pe/profFacultad.php?flag=profFacu&facul=" + facultades[i] + "&pag=" + str(j+1)
+            response = r.get(url, cookies=cookies, verify=False)
+            response.encoding = 'utf-8'
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for link in soup.find_all('td'):
+                if(link.get('width') == "79"):
+                    print(str(link.contents[0]))
+                    codigos.append(str(link.contents[0]))
+
+    for i in range(0,len(codigos)):
+        with open("codigos.txt", "a+", encoding="utf-8") as file:
+            file.write(codigos[i] + "\n")
+
+# SEGUNDA PARTE: UTILIZANDO LOS CÓDIGOS PARA HALLAR TODOS LOS DATOS
+
+# https://www.orce.uni.edu.pe/detaalu.php?id=07686040&op=detdoc
+
+
+
 
 
 # . <- navigable object
@@ -31,6 +45,7 @@ for i in range(0,len(facultades)):
 # contents
 
 #print(soup.table.tbody)
+"""
 print("--------------------------------------------")
 #print(soup.tbody)
 #print("THEY'RE THE SAME")
@@ -41,3 +56,4 @@ for x in soup.tbody.find_all('tr')[3:10]:
   if len(tds)>1:
     print(tds[1].string)
 
+"""
