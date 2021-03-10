6c014b08c2f4a9a31f7c44f9d317f13301cc6adc
TheBestShooter <rnolascoc@uni.pe>
2021-03-10 03:01:46 -0500

    utilizando cookies para validar sesion

--- scrap.py
+++ scrap.py
@@ -1,11 +1,20 @@
+import requests as r
+from bs4 import BeautifulSoup
+import urllib3
+import json
 
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 #Hallando los códigos de todos los profes
 facultades = ['A','C','E','G','I','L','M','N','P','Q','S']
 hojasPorFacultad = [19,30,20,16,19,21,23,21,9,13,19]
 
-for i in range(0,len(facultades)):
-    for j in range(0,hojasPorFacultad[i]):
-        print (facultades[i] + " - " + str(j+1))
+url = "https://www.orce.uni.edu.pe/profFacultad.php?flag=profFacu&facul=A&pag=1"
 
+cookies = dict(PHPSESSID='4pcpkms76g5r9f8jki1a5s7292')
 
+response = r.get(url, cookies=cookies)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
