87dbd8f73cbf3b3b62c731c1269669353cfa090c
TheBestShooter <rnolascoc@uni.pe>
2021-03-11 00:54:39 -0500

    consiguiendo urls de las imagenes

--- scrap2.py
+++ scrap2.py
@@ -7,17 +7,28 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 urls = []
 
-for i in range(102,238):
-    print(i)
-    url = "http://www.fiis.uni.edu.pe/plana-docente/" + str(i)
-    response = r.get(url, verify=False)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
+def obteniendoImagenes():
+    for i in range(0,127,18):
+        url = "http://www.fiis.uni.edu.pe/plana-docente?start=" + str(i)
+        response = r.get(url, verify=False)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    if(len(soup.find_all('img')) > 2):
-        if (str(soup.find_all('img')[2]["src"]) != "/images/Docentes/docente.jpg"):
-            urls.append("http://www.fiis.uni.edu.pe" + str(soup.find_all('img')[2]["src"]))
+        for i in range (2,20-(i==126)*9):
+            if (str(soup.find_all('img')[i]["src"]) != "/images/Docentes/docente.jpg"):
+                urls.append("http://www.fiis.uni.edu.pe" + str(soup.find_all('img')[i]["src"]))
 
     with open("urls.txt", "w", encoding="utf-8") as file:
         for i in range(0,len(urls)):
             file.write(urls[i] + "\n")
+
+def creandoJson():
+    print("xd")
+
+
+def main():
+    obteniendoImagenes()
+    #creandoJson()
+
+if __name__ == "__main__":
+    main()
