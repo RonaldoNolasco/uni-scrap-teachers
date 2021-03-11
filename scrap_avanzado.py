d10ab6eb11cb98da8407358474f4b864f49e0b15
TheBestShooter <rnolascoc@uni.pe>
2021-03-11 01:00:52 -0500

    ajustando nombres

--- scrap_avanzado.py
+++ scrap_avanzado.py
@@ -15,20 +15,30 @@ def obteniendoImagenes():
         soup = BeautifulSoup(response.text, 'html.parser')
 
         for i in range (2,20-(i==126)*9):
-            if (str(soup.find_all('img')[i]["src"]) != "/images/Docentes/docente.jpg"):
+            if (str(soup.find_all('img')[i]["src"]) != "/images/Docentes/docente.jpg") and (str(soup.find_all('img')[i]["src"]) != "/images/Docentes/luis_zuloaga.jpg"):
                 urls.append("http://www.fiis.uni.edu.pe" + str(soup.find_all('img')[i]["src"]))
 
     with open("urls.txt", "w", encoding="utf-8") as file:
         for i in range(0,len(urls)):
             file.write(urls[i] + "\n")
 
+def almacenarImagenes():
+    with open("urls.txt", "r", encoding="utf-8") as file:
+        for value in file.read().split('\n'):
+            urls.append(value)
+
+    urls.pop(len(urls)-1)
+
 def creandoJson():
     print("xd")
 
 
 def main():
-    obteniendoImagenes()
+    #obteniendoImagenes()
+    almacenarImagenes()
     #creandoJson()
+
+    print(len(urls))
 
 if __name__ == "__main__":
     main()
